30fb46978a919a57f9b79b772bd007aabbbc5eec
nguyenha <nguyenha2651988@gmail.com>
2019-05-31 13:01:15 +0700

    [Bài tập] Máy tính đơn giản

---
+++ baitap.js
@@ -0,0 +1,51 @@
+function a1() {
+    let textbox = document.getElementById("text");
+    textbox.value += "1";}
+function a2() {
+    let textbox = document.getElementById("text");
+    textbox.value += "2";}
+function a3() {
+    let textbox = document.getElementById("text");
+    textbox.value += "3";}
+function a5() {
+    let textbox = document.getElementById("text");
+    textbox.value += "4";}
+function a6() {
+    let textbox = document.getElementById("text");
+    textbox.value += "5";}
+function a7() {
+    let textbox = document.getElementById("text");
+    textbox.value += "6";}
+function a9() {
+    let textbox = document.getElementById("text");
+    textbox.value += "7";}
+function a10() {
+    let textbox = document.getElementById("text");
+    textbox.value += "8";}
+function a11() {
+    let textbox = document.getElementById("text");
+    textbox.value += "9";}
+function a14() {
+    let textbox = document.getElementById("text");
+    textbox.value += "0";}
+function a4() {
+    let textbox = document.getElementById("text");
+    textbox.value += "+";}
+function a8() {
+    let textbox = document.getElementById("text");
+    textbox.value += "-";}
+function a12() {
+    let textbox = document.getElementById("text");
+    textbox.value += "*";}
+function a16() {
+    let textbox = document.getElementById("text");
+    textbox.value += "/";}
+function a17() {
+    let textbox = document.getElementById("text");
+    textbox.value += ".";}
+function a15() {
+    let textbox = document.getElementById("text");
+    textbox.value = eval(textbox.value)}
+function a13() {
+    let textbox = document.getElementById("text");
+    textbox.value = " ";}
